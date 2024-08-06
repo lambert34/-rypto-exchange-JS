932cc1e4bcf89817a20cf266cf2dbe50e3affd13
alexL <aleksandrlambert@gmail.com>
2024-08-06 15:52:42 +0700

    добавляет код управления навигацией
    переключает видимость навигационного меню при клике на кнопку-тогглер.

---
+++ Cryptex/assets/js/script.js
@@ -0,0 +1,22 @@
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+    document.body.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, 'click', toggleNavbar); //2636
