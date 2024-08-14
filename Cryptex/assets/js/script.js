ce4efbd864fb802d459944fd67a45041cff5ebe5
alexL <aleksandrlambert@gmail.com>
2024-08-14 13:16:13 +0700

    добавляет "ленивый" скролл
    функция реализует плавное появление секций при прокрутке экрана для красивого UXэффекта

--- Cryptex/assets/js/script.js
+++ Cryptex/assets/js/script.js
@@ -48,3 +48,20 @@ const toggleActive = function () {
 }
 
 addEventOnElem(addToFavBtns, "click", toggleActive);
+
+// тут будет эффект скролла
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+            sections[i].classList.add("active");
+        } else {
+            sections[i].classList.remove("active");
+        }
+    }
+}
+
+scrollReveal();
+addEventOnElem(window, "scroll", scrollReveal);
